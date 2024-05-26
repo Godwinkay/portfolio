a4f1e2a13bd3e8e24ceea1756e7767d0bc390d48
Godwinkay <117829382+Godwinkay@users.noreply.github.com>
2024-05-26 19:54:17 +0000

    worked on responsiveness

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,15 +9,15 @@ const Projects = () => {
   return (
     <div className="px-16 lg:px-24 flex flex-col gap-8 items-center bg-black text-white py-16">
       <h2 className="text-4xl font-serif font-bold mb-8">Projects</h2>
-      <div className="lg:flex gap-12 lg:justify-around mb-12">
-        <div className="space-y-2 relative h-[200px] lg:h-max-fit">
+      <div className="lg:flex gap-12 lg:justify-around mb-12 space-y-4">
+        <div className="space-y-2">
           <h3 className="font-bold">ThriftWear E-Commerce Website</h3>
           <p className="text-justify">
             This is a Website where you could purchase second hand clothing,
             bags and shoes. It has a shop page where items can be selected by
             category and a function cart page. 
           </p>
-          <div className="flex gap-x-12 items-end absolute left-0 bottom-4">
+          <div className="flex gap-x-12 items-end mt-4">
           <a href="https://github.com/Godwinkay/thriftware.git" target="_blank" rel="noopener noreferrer">< FaSquareGithub size={30} /> </a>
             <a href="https://thriftwear.netlify.app/" target="_blank" rel="noopener noreferrer"><p className="flex items-baseline gap-x-2">View Website <BsBoxArrowUpRight /></p> </a>
           </div>
@@ -28,13 +28,13 @@ const Projects = () => {
         </div>
       </div>
 
-      <div className="lg:flex lg:flex-row-reverse gap-12 lg:justify-around mb-12">
-        <div className="space-y-2 relative h-[200px] lg:h-max-fit">
+      <div className="lg:flex lg:flex-row-reverse gap-12 lg:justify-around mb-12 space-y-4">
+        <div className="space-y-2">
           <h3 className="font-bold">Car Dealership Website</h3>
           <p className="text-justify">
           Our all-in-one automotive platform offers a seamless shopping and service experience. Rent vehicles for short or long-term, shop genuine spare parts, and access our expert repair services - all through a single, user-friendly website. 
           </p>
-          <div className="flex gap-x-12 items-end absolute left-0 bottom-4">
+          <div className="flex gap-x-12 items-end mt-4">
             <a href="https://github.com/Godwinkay/Car-Dealership--Tailwind-React.git" target="_blank" rel="noopener noreferrer">< FaSquareGithub size={30} /> </a>
             <a href="https://yourcarguy.netlify.app/" target="_blank" rel="noopener noreferrer"><p className="flex items-baseline gap-x-2">View Website <BsBoxArrowUpLeft /></p></a>
           </div>
